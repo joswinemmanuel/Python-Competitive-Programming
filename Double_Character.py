14f924ef2ac9dd9ef10675030b615e22b0469925
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-02-16 14:48:50 +0530

    Return a string where every character is doubled

---
+++ Double_Character.py
@@ -0,0 +1,17 @@
+def doubleChar(str_data):
+   res = ''
+   for i in str_data:
+      res += i*2
+   return res
+
+print(doubleChar('Hi-There'))
+print(doubleChar('OKOKOK'))
+print(doubleChar('Joswin'))
+print(doubleChar('WooYear'))
+
+'''
+HHii--TThheerree
+OOKKOOKKOOKK
+JJoosswwiinn
+WWooooYYeeaarr
+'''
