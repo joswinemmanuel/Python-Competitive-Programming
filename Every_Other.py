c81171da09fce14a8b0391350f15526994fd74c1
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-02-16 14:36:59 +0530

    To get every other letters in a string

---
+++ Every_Other.py
@@ -0,0 +1,12 @@
+def everyOther(string_data):
+   return string_data[::2]
+
+print(everyOther("Joswin"))
+print(everyOther("Hieolclvo"))
+print(everyOther("Pop Smoke"))
+
+'''
+Jsi
+Hello
+PpSoe
+'''
